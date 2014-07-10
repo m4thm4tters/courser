ddd50f889a8d7d2d595b9161b4a3f719db39ba3e
Justin Keller <m4thm4tters@gmail.com>
2014-07-09 22:33:57 -0400

    change announcement form in active admin

--- app/admin/announcement.rb
+++ app/admin/announcement.rb
@@ -13,5 +13,14 @@ ActiveAdmin.register Announcement do
   #  permitted << :other if resource.something?
   #  permitted
   # end
+
+  form do |f|
+    f.inputs do
+      f.input :course
+      f.input :date, :as => :datepicker
+      f.input :title
+      f.input :content
+    end
+  end
   
 end
